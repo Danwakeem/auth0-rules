65370bbfcb2e1f3fc49d16f5130e101202afb81f
Danwakeem <danwakeem@gmail.com>
2019-08-19 20:48:26 -0500

    Updated log rule

--- rules/log.js
+++ rules/log.js
@@ -1,13 +1,39 @@
-const Rollbar = require('rollbar');
-
 function rule(user, context, callback) {
-  const rollbar = new Rollbar({
-    accessToken: configuration.rollbarToken,
-    captureUncaught: true,
-    captureUnhandledRejections: true
-  });
-  rollbar.info('New User', user);
-  console.log('Hello Jose');
+  const request = require('request');
+  const postToRollbar = person => {
+    const now = new Date();
+    const options = {
+      method: 'POST',
+      url: 'https://api.rollbar.com/api/1/item/',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        access_token: configuration.rollbarToken,
+        data: {
+          level: 'info',
+          environment: 'test',
+          timestamp: now.getTime(),
+          platform: 'linux',
+          language: 'node.js',
+          person: {
+            id: '1234',
+            username: person.name,
+            email: person.email
+          },
+          title: 'Test Rollbar From Auth0',
+          body: {
+            message: {
+              body: 'Test Rollbar From Auth0',
+              route: 'rule/log-test'
+            }
+          }
+        }
+      })
+    };
+
+    request(options);
+  };
+
+  postToRollbar(user);
   callback(null, user, context);
 }
 
